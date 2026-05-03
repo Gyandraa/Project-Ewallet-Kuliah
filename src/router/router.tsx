e47fa959369dc02b8e457ac57921233e122ca778
gyandra <gyandranaupal14@gmail.com>
2026-05-03 13:01:44 +0700

    feat: add import pakedatapages

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,9 +3,10 @@ import { Routes } from "react-router";
 import Home from "../pages/Home";
 import TransferPages from "../pages/transferPages";
 import TopupPages from "../pages/topupPages";
-import PaketData from "../pages/PaketData";
 import ScanQrPages from "../pages/scanQrPages";
 import InvestasiPages from "../pages/investasiPages";
+import AktivitasSection from "../pages/aktivitasSection";
+import PaketData from "../pages/paketData";
 export default function Router() {
   return (
     <>
@@ -13,9 +14,10 @@ export default function Router() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/transfer" element={<TransferPages />}></Route>
         <Route path="/topup" element={<TopupPages />}></Route>
-        <Route path="/paket-data" element={<PaketData />}></Route>
         <Route path="/scan" element={<ScanQrPages />}></Route>
         <Route path="/investasi" element={<InvestasiPages />}></Route>
+        <Route path="/activity" element={<AktivitasSection />}></Route>
+        <Route path="/paket-data" element={<PaketData />}></Route>
       </Routes>
     </>
   );
